9bd2254e6effb5e5bf874e8357a0e5f1f21ece58
GmRabbany <129893569+GmRabbany@users.noreply.github.com>
2023-07-02 20:22:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+function thirdFunction(){
+    document.getElementById("changeParagraph3").innerHTML="paragraph changed by using function in external Js, to know more visit the script.js file";
+}
+
+function fourthFunction(){
+    document.getElementById("changeParagraph3").innerHTML="A Paragraph.";
+}
